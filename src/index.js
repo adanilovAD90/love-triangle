6e4e6821de291fac2997b95db9b91cd76c2aa99a
adanilovAD90 <antona90@gmail.c>
2019-03-13 15:42:25 +0300

    feat:add implementation

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let i = 0, lenght = preferences.length; i < lenght; i++) {
+    const l1 = preferences[preferences[preferences[i] - 1] - 1] === i + 1;
+    const l2 = i + 1 !== preferences[i];
+    if (l1 && l2) {
+        count++;
+    }
+  }
+  return count / 3;
 };
